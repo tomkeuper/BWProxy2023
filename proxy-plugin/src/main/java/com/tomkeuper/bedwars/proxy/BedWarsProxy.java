b2717007fc0b2982203cdf28a1b1d0644fe86c3e
Tom Keuper <tomkeuper@live.nl>
2023-11-21 23:14:53 +0100

    Update bstats id

--- proxy-plugin/src/main/java/com/tomkeuper/bedwars/proxy/BedWarsProxy.java
+++ proxy-plugin/src/main/java/com/tomkeuper/bedwars/proxy/BedWarsProxy.java
@@ -166,7 +166,7 @@ public class BedWarsProxy extends JavaPlugin {
             isPapi = true;
         }
 
-        Metrics m = new Metrics(this, 6036);
+        Metrics m = new Metrics(this, 20358);
         m.addCustomChart(new SimplePie("default_language", () -> LanguageManager.get().getDefaultLanguage().getIso()));
         m.addCustomChart(new SimplePie("party_adapter", () -> getParty().getClass().getName()));
         m.addCustomChart(new SimplePie("level_adapter", () -> getLevelManager().getClass().getName()));
