2841b3438499ea63be0e0a2556058e86c06b6b8f
Tom Keuper <tomkeuper@live.nl>
2023-11-21 23:43:09 +0100

    Use simplename for metrics

--- proxy-plugin/src/main/java/com/tomkeuper/bedwars/proxy/BedWarsProxy.java
+++ proxy-plugin/src/main/java/com/tomkeuper/bedwars/proxy/BedWarsProxy.java
@@ -168,8 +168,8 @@ public class BedWarsProxy extends JavaPlugin {
 
         Metrics m = new Metrics(this, 20358);
         m.addCustomChart(new SimplePie("default_language", () -> LanguageManager.get().getDefaultLanguage().getIso()));
-        m.addCustomChart(new SimplePie("party_adapter", () -> getParty().getClass().getName()));
-        m.addCustomChart(new SimplePie("level_adapter", () -> getLevelManager().getClass().getName()));
+        m.addCustomChart(new SimplePie("party_adapter", () -> getParty().getClass().getSimpleName()));
+        m.addCustomChart(new SimplePie("level_adapter", () -> getLevelManager().getClass().getSimpleName()));
         SignManager.init();
     }
 
