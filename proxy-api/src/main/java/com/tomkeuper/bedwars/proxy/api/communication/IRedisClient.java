d5d58edbd4195571d80294416495739b281c5264
MrCeasar <tomkeuper@live.nl>
2024-04-20 23:44:46 +0200

    Add redis setting method to api

--- proxy-api/src/main/java/com/tomkeuper/bedwars/proxy/api/communication/IRedisClient.java
+++ proxy-api/src/main/java/com/tomkeuper/bedwars/proxy/api/communication/IRedisClient.java
@@ -5,4 +5,13 @@ import com.google.gson.JsonObject;
 
 public interface IRedisClient {
     void sendMessage(@NotNull JsonObject message, @NotNull String addonIdentifier);
+
+    /**
+     * Retrieve the data associated with a specific identifier from the Redis database.
+     * Settings can be stored by the BedWars plugin.
+     *
+     * @param redisSettingIdentifier the identifier of the setting to be checked.
+     * @return the data as a string associated with the specified identifier
+     */
+    String retrieveSetting(@NotNull String redisSettingIdentifier);
 }
